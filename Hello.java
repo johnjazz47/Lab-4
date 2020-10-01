efabfccd533daf603175b9266648466c377478c7
John Jasinski <johnjazz@umich.edu>
2020-10-01 11:43:33 -0400

    Added by John Jasinski

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
   public static void main(String args[]){
     for(int i = 1; i<=10; i++){
-      System.out.println("Hello World" + i)
+      System.out.println("Hello World" + i);
     }//end loop
+    System.out.println("by John Jasinski");
   }//end main
 }//end Hello
